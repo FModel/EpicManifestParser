21df8a55d474f14148a35bc943e06f3fdc20c997
4sval <asval.contactme@gmail.com>
2023-05-10 16:27:04 +0200

    downloadable + content manifest info

--- src/EpicManifestParser/Objects/ManifestInfo.cs
+++ src/EpicManifestParser/Objects/ManifestInfo.cs
@@ -2,15 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace EpicManifestParser.Objects
 {
-    public class ManifestInfo
+    public class ManifestInfo : DownloadableManifestInfo
     {
         public string AppName { get; }
         public string LabelName { get; }
@@ -18,8 +15,6 @@ namespace EpicManifestParser.Objects
         public Version Version { get; }
         public int CL { get; }
         public string Hash { get; }
-        public List<UriBuilder> Uris { get; }
-        public string FileName { get; }
 
         public ManifestInfo() { }
         public ManifestInfo(Stream jsonStream, int idx = 0) : this(JsonDocument.Parse(jsonStream), idx) { }
@@ -35,16 +30,14 @@ namespace EpicManifestParser.Objects
             Hash = rootElement.GetProperty("hash").GetString();
 
             var buildMatch = Regex.Match(BuildVersion, @"(\d+(?:\.\d+)+)-CL-(\d+)", RegexOptions.Singleline);
-
             if (buildMatch.Success)
             {
                 Version = Version.Parse(buildMatch.Groups[1].Value);
                 CL = int.Parse(buildMatch.Groups[2].Value);
             }
 
-            var manifestsArray = rootElement.GetProperty("manifests");
             var manifestUriBuilders = new List<UriBuilder>();
-
+            var manifestsArray = rootElement.GetProperty("manifests");
             foreach (var manifestElement in manifestsArray.EnumerateArray())
             {
                 var uriBuilder = new UriBuilder(manifestElement.GetProperty("uri").GetString());
@@ -72,81 +65,6 @@ namespace EpicManifestParser.Objects
 
             Uris = manifestUriBuilders;
             FileName = Uris.FirstOrDefault()?.Uri.Segments[^1];
-        }
-
-        public byte[] DownloadManifestData(string cacheDir = null)
-        {
-            var path = cacheDir == null ? null : Path.Combine(cacheDir, FileName);
-
-            if (path != null && File.Exists(path))
-            {
-                return File.ReadAllBytes(path);
-            }
-
-            using var client = new HttpClient();
-            byte[] data = null;
-
-            foreach (var uri in Uris.TakeWhile(_ => data == null))
-            {
-                try
-                {
-                    data = client.GetByteArrayAsync(uri.Uri).GetAwaiter().GetResult();
-                }
-                catch
-                {
-                    data = null;
-                }
-            }
-
-            if (path != null && data != null)
-            {
-                File.WriteAllBytes(path, data);
-            }
-
-            return data;
-        }
-
-        public string DownloadManifestString(string cacheDir = null)
-        {
-            return Encoding.UTF8.GetString(DownloadManifestData(cacheDir));
-        }
-
-        public async Task<byte[]> DownloadManifestDataAsync(string cacheDir = null)
-        {
-            var path = cacheDir == null ? null : Path.Combine(cacheDir, FileName);
-
-            if (path != null && File.Exists(path))
-            {
-                return await File.ReadAllBytesAsync(path).ConfigureAwait(false);
-            }
-
-            using var client = new HttpClient();
-            byte[] data = null;
-
-            foreach (var uri in Uris.TakeWhile(_ => data == null))
-            {
-                try
-                {
-                    data = await client.GetByteArrayAsync(uri.Uri).ConfigureAwait(false);
-                }
-                catch
-                {
-                    data = null;
-                }
-            }
-
-            if (path != null && data != null)
-            {
-                await File.WriteAllBytesAsync(path, data).ConfigureAwait(false);
-            }
-
-            return data;
-        }
-
-        public async Task<string> DownloadManifestStringAsync(string cacheDir = null)
-        {
-            var data = await DownloadManifestDataAsync(cacheDir).ConfigureAwait(false);
-            return Encoding.UTF8.GetString(data);
         }
     }
 }
